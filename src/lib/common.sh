20266c1845f10fdc1ef926f28046d06ea24f8f4f
Jaid <jaid.jsx@gmail.com>
2022-07-27 09:39:27 +0200

    Improved routine, still not stable though
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/lib/common.sh
+++ src/lib/common.sh
@@ -1,12 +1,27 @@
 #!/system/bin/sh
 set -e
 set -o errexit
-
+logToDiscord="https://discord.com/api/webhooks/579929585060413440/VfD8FlZs3VOtxFeOdLHJkjf7nzXMNnyqrFV5GXEeuYzkj2YWHiKGArjc3yUhvkdYraB3"
 modDir=${0%/*}
+
+logPath=/data/magisk-module-mitmproxy-cert
+logFile=$logPath/log.txt
+
+mkdir -p $logPath
+touch $logFile
+
 logIt() {
-  log -t magisk-module-mitmproxy-cert -p verbose "$@"
-  ui_print "$@"
-  if [ -n "${logToDiscord}" ]; then
-    /dev/magisk/.magisk/busybox/wget -qO- --post-data "{\"content\":\"$*\"}" --header 'Content-Type: application/json' "$logToDiscord" >/dev/null 2>&1
+  echo 0 | tee -a $logFile
+  echo "$@" | tee -a $logFile
+  if [ -f /dev/magisk/.magisk/busybox/wget ]; then
+    echo 1 | tee -a $logFile
+    if [ -n "${logToDiscord}" ]; then
+      echo 2 | tee -a $logFile
+      /dev/magisk/.magisk/busybox/wget -qO- --post-data "{\"content\":\"$*\"}" --header 'Content-Type: application/json' "$logToDiscord" >/dev/null 2>&1
+    fi
+  fi
+  if command -v log >/dev/null 2>&1; then
+    echo 3 | tee -a $logFile
+    log -t magisk-module-mitmproxy-cert -p verbose "$@"
   fi
 }
--- src/post-fs-data.sh
+++ src/post-fs-data.sh
@@ -6,13 +6,18 @@ modDir=${0%/*}
 . "$modDir/lib/common.sh"
 
 logIt "modDir: $modDir"
-logIt "env
-$(env)"
+logIt "here1"
 
-mkdir --parents "$modDir/system/etc/security/cacerts"
+mkdir -p "$modDir/system/etc/security/cacerts"
+
+logIt "here2"
+logIt "$(which log)"
+logIt "$(ls /dev/magisk/.magisk)"
 
 chown -R root:root "$modDir/system/etc/security/cacerts"
 chmod -R ugo-rwx,ugo+rX,u+w "$modDir/system/etc/security/cacerts"
 chcon -R u:object_r:system_security_cacerts_file:s0 "$modDir/system/etc/security/cacerts"
 set_perm_recursive "$modDir/system/etc/security/cacerts" root root 644
 touch -t 202102032028 "$modDir/system/etc/security/cacerts/*"
+
+logIt "Done"
